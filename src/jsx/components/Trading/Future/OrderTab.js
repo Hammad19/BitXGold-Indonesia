623f87a8ace4182e363f057520329051252c4166
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2023-04-03 00:16:04 +0500

    Update OrderTab.js

--- src/jsx/components/Trading/Future/OrderTab.js
+++ src/jsx/components/Trading/Future/OrderTab.js
@@ -82,12 +82,12 @@ const OrderTab = ({ acceptedData, to, handleConnectClick }) => {
                     {state.auth.auth.isAdmin ? (
                       <Link
                         onClick={() => {
-                          handleConnectClick(item.wallet_address);
+                          handleConnectClick(item.user_id);
                         }}>
-                        {item.wallet_address}
+                        {item.user.wallet_public_key}
                       </Link>
                     ) : (
-                      item.wallet_address
+                      item.user.wallet_public_key
                     )}
                   </td>
                   {/* <td>{item.blockhash}</td> */}
