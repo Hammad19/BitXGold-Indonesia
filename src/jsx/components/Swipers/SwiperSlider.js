f444388c3ea7cb13262f239a327c27b6cf693824
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2023-04-03 00:16:10 +0500

    Update SwiperSlider.js

--- src/jsx/components/Swipers/SwiperSlider.js
+++ src/jsx/components/Swipers/SwiperSlider.js
@@ -8,84 +8,124 @@ const SwiperSlider = ({
   referralBonus,
   stakingreferralBonus,
   totalEarning,
+  usdt,
+  bnb,
 }) => {
   return (
-    <div className="col-xl-12">
-      <Swiper
-        className="mySwiper"
-        speed={1500}
-        slidesPerView={5}
-        spaceBetween={20}
-        loop={false}
-        //autoplay= {{
-        //delay: 1200,
-        //}}
-        //modules={[ Autoplay ]}
-        breakpoints={{
-          300: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-          },
-          416: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-          },
-          1200: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
+    <>
+      <div className="col-xl-12">
+        <Swiper
+          className="mySwiper"
+          speed={1500}
+          slidesPerView={3}
+          spaceBetween={20}
+          loop={false}
+          //autoplay= {{
+          //delay: 1200,
+          //}}
+          //modules={[ Autoplay ]}
+          breakpoints={{
+            300: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            },
+            416: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+          }}>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={bxgavailable}
+              translateKey="dashboard_card_1"
+              currency="BXG"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={bxgstacked}
+              translateKey="dashboard_card_2"
+              currency=" BXG"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={Math.round(referralBonus * 100) / 100}
+              translateKey="dashboard_card_3"
+              currency=" USDT"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="col-xl-12">
+        <Swiper
+          className="mySwiper"
+          speed={1500}
+          slidesPerView={4}
+          spaceBetween={20}
+          loop={false}
+          //autoplay= {{
+          //delay: 1200,
+          //}}
+          //modules={[ Autoplay ]}
+          breakpoints={{
+            300: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            },
+            416: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1200: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={Math.round(stakingreferralBonus * 100) / 100}
+              translateKey="dashboard_card_4"
+              currency=" BXG"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={totalEarning}
+              translateKey="dashboard_card_5"
+              currency=" BXG"
+            />
+          </SwiperSlide>
 
-          1500: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-          },
-          1788: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-          },
-        }}>
-        <SwiperSlide>
-          <SwiperSlideCard
-            amount={bxgavailable}
-            translateKey="dashboard_card_1"
-            currency="BXG"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <SwiperSlideCard
-            amount={bxgstacked}
-            translateKey="dashboard_card_2"
-            currency=" BXG"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <SwiperSlideCard
-            amount={Math.round(referralBonus * 100) / 100}
-            translateKey="dashboard_card_3"
-            currency=" USDT"
-          />
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <SwiperSlideCard
-            amount={Math.round(stakingreferralBonus * 100) / 100}
-            translateKey="dashboard_card_4"
-            currency=" BXG"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <SwiperSlideCard
-            amount={totalEarning}
-            translateKey="dashboard_card_5"
-            currency=" BXG"
-          />
-        </SwiperSlide>
-      </Swiper>
-    </div>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={usdt}
+              translateKey="dashboard_card_6"
+              currency="USDT"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SwiperSlideCard
+              amount={bnb}
+              translateKey="dashboard_card_7"
+              currency="BNB"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
   );
 };
 
