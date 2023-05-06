e6a9b61a6dc3b03506addf9c1b793bef3dd8027a
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2023-05-06 12:43:14 +0500

    Update EditProfile.js

--- src/jsx/layouts/EditProfile.js
+++ src/jsx/layouts/EditProfile.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 //import { Dropdown } from 'react-bootstrap';
-import { Link } from "react-router-dom";
-import { connect, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { connect, useDispatch, useSelector } from "react-redux";
 import toast, { Toaster } from "react-hot-toast";
 import axiosInstance from "../../services/AxiosInstance";
 import { ThemeContext } from "../../context/ThemeContext";
 import { useTranslation } from "react-i18next";
 
 import CountryCodePicker from "../components/PhoneInput/CountryCodePicker";
+import { Logout } from "../../store/actions/AuthActions";
 
 const EditProfile = (props) => {
   const state = useSelector((state) => state);
@@ -21,64 +22,147 @@ const EditProfile = (props) => {
     id: "",
     email: "",
     contact: "",
+    old_wallet_public_key: "",
   });
   const [email, setemail] = useState("");
   const [whatsapp, setwhatsapp] = useState("");
+  const [oldWalletAddress, setOldWalletAddress] = useState("");
 
   const getdata = async () => {
     const { data } = await axiosInstance.get(
       "/api/profile/" + state.auth.userDetails.id
     );
+
+    setprofileData({
+      id: data.id,
+      email: data.email,
+      contact: data.contact,
+      old_wallet_public_key: data.old_wallet_public_key,
+    });
     setprofileData(data);
     setemail(data.email);
     setwhatsapp(data.contact);
+    setOldWalletAddress(data.old_wallet_public_key);
   };
+
+  async function verifyOldWalletPublicKey() {
+    const requestBody = {
+      wallet_address: oldWalletAddress,
+    };
+
+    const { data } = await axiosInstance
+      .post(
+        "/api/auth/verifywallet",
+
+        requestBody,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      )
+      .catch((err) => {
+        toast.error("The old Wallet Address You Entered does not exist", {
+          position: "top-center",
+        });
+        return false;
+      });
+
+    //console.log(data, "data");
+    if (data) {
+      return data.status;
+    }
+  }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   async function update() {
     try {
-      if (email === "" || whatsapp === "") {
+      if (email === "" || whatsapp === "" || oldWalletAddress === "") {
         toast.error("Please Enter Required Fileds", {
           style: { minWidth: 180 },
           position: "top-center",
         });
       } else {
-        const mesg = await axiosInstance.put(
-          "/api/profile/" + state.auth.userDetails.id,
-          {
-            email: email,
-            contact: whatsapp,
-          }
-        );
-        if (mesg.data === "updated") {
-          toast.success("updated successfully ", {
-            style: { minWidth: 180 },
-            position: "top-center",
-          });
-          getdata();
-        } else {
-          if (mesg.data.message === "User Not Found With The Given Id.") {
-            const response = await axiosInstance.put(
-              "/api/profile/" + state.auth.userDetails.id,
-              {
-                email: email,
-                contact: whatsapp,
-              }
-            );
-            console.log(response);
+        const verifywalletstatus = await verifyOldWalletPublicKey();
 
-            if (response.status === 200) {
-              toast.success("updated successfully ", {
+        const changedfields = {};
+        if (profileData.email !== email) {
+          changedfields.email = email;
+        }
+        if (profileData.contact !== whatsapp) {
+          changedfields.contact = whatsapp;
+        }
+        if (profileData.old_wallet_public_key !== oldWalletAddress) {
+          changedfields.old_wallet_public_key = oldWalletAddress;
+        }
+        const requestBody = {
+          ...changedfields,
+        };
+
+        console.log(requestBody, "Print this Bro");
+
+        if (verifywalletstatus) {
+          const mesg = await axiosInstance.put(
+            "/api/profile/" + state.auth.userDetails.id,
+            requestBody
+          );
+
+          if (mesg.data === "values updated") {
+            toast.success("updated successfully ", {
+              style: { minWidth: 180 },
+              position: "top-center",
+            });
+            //if old wallet is changed logout else call getdata
+            if (profileData.old_wallet_public_key !== oldWalletAddress) {
+              //show toast for logging out because old wallet changed
+              toast.success("Old Wallet Address Changed! Please Login Again", {
                 style: { minWidth: 180 },
                 position: "top-center",
               });
-              getdata();
+              //settimeout for 3 seconds to log out
+
+              setTimeout(() => {
+                dispatch(Logout(navigate));
+              }, 2000);
             } else {
-              toast.error("Something Went Wrong", {
-                style: { minWidth: 180 },
-                position: "top-center",
-              });
+              console.log("else");
+              getdata();
+            }
+
+            if (mesg.data.message === "User Not Found With The Given Id.") {
+              const response = await axiosInstance.put(
+                "/api/profile/" + state.auth.userDetails.id,
+                {
+                  email: email,
+                  contact: whatsapp,
+                  old_wallet_public_key: oldWalletAddress,
+                }
+              );
+              console.log(response);
+
+              if (response.status === 200) {
+                toast.success("updated successfully ", {
+                  style: { minWidth: 180 },
+                  position: "top-center",
+                });
+                getdata();
+              } else {
+                toast.error("Something Went Wrong", {
+                  style: { minWidth: 180 },
+                  position: "top-center",
+                });
+              }
             }
           }
+        } else {
+          toast.error(
+            "This Old Wallet Address does not exist in BitXGold Malaysia",
+            {
+              style: { minWidth: 180 },
+              position: "top-center",
+            }
+          );
         }
       }
     } catch (err) {
@@ -161,6 +245,18 @@ const EditProfile = (props) => {
                       className="form-control"
                       value={email}
                       onChange={(e) => setemail(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="col-sm-6 m-b30">
+                    <label className="form-label">
+                      {t("Old Wallet Address")}
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={oldWalletAddress}
+                      onChange={(e) => setOldWalletAddress(e.target.value)}
                     />
                   </div>
                 </div>
